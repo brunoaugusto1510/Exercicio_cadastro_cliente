43eb7360018e2ed02775085015459316b69e0f6c
Bruno Augusto <brunoaugustomaciel@gmail.com>
2026-03-24 20:05:58 -0300

    fix - css

--- quinto_exemplo/form.php
+++ quinto_exemplo/form.php
@@ -27,7 +27,7 @@ $categorias = $stmtCategorias->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <title><?= $produto['id'] ? 'Editar' : 'Incluir' ?> Produto</title>
-    <link rel="stylesheet" href="estilo_form.css">
+    <link rel="stylesheet" href="../css/estilo_form.css">
 </head>
 <body>
 
